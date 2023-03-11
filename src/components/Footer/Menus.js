1a40f812edd5f65c5f74c72925c0b21460fc56dc
Naval <naval19065@iiitd.ac.in>
2023-03-11 19:58:57 +0530

    footer template added

---
+++ src/components/Footer/Menus.js
@@ -0,0 +1,32 @@
+export const PRODUCTS = [
+    {name: 'Drag and Drop', link: '#'},
+    {name: 'Visual Studio X', link: '#'},
+    {name: 'Easy Content', link: '#'},
+];
+export const RESOURCES = [
+    {name: 'Industries and tools', link: '#'},
+    {name: 'Use cases', link: '#'},
+    {name: 'Blog', link: '#'},
+    {name: 'Online event', link: '#'},
+    {name: 'Nostrud exercitation', link: '#'},
+];
+export const COMPANY = [
+    {name: 'Diversity & inclusion', link: '#'},
+    {name: 'About us', link: '#'},
+    {name: 'Press', link: '#'},
+    {name: 'Customer stories', link: '#'},
+    {name: 'Online communities', link: '#'},
+];
+export const SUPPORT = [
+    {name: 'Documentation', link: '#'},
+    {name: 'Tutorials & guides', link: '#'},
+    {name: 'Webinars', link: '#'},
+    {name: 'Open-source', link: '#'},
+];
+export const Icons = [
+    {name: 'logo-facebook', link: '#'},
+    {name: 'logo-twitter', link: '#'},
+    {name: 'logo-github', link: '#'},
+    {name: 'logo-linkedin', link: '#'},
+    {name: 'logo-instagram', link: '#'},
+];
